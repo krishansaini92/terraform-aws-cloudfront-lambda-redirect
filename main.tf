884370d853d284587793903a41067808a5655861
Krishan Saini <krishan@thinkscoopinc.com>
2023-07-11 22:59:32 -0400

    Fix for ACL changes

--- main.tf
+++ main.tf
@@ -90,23 +90,23 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
-  depends_on = [
-    aws_s3_bucket.this
-  ]
-}
-
 resource "aws_s3_bucket_ownership_controls" "this" {
   bucket = aws_s3_bucket.this.id
 
   rule {
-    object_ownership = "BucketOwnerEnforced"
+    object_ownership = "BucketOwnerPreferred"
   }
   depends_on = [
     aws_s3_bucket.this,
-    aws_s3_bucket_acl.this
+  ]
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+  depends_on = [
+    aws_s3_bucket.this,
+    aws_s3_bucket_ownership_controls.this
   ]
 }
 
